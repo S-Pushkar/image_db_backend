77282e56768f781cdf91ba67d03bd45c9e14503f
Pushkar S <spushkar0071234@gmail.com>
2024-10-06 15:43:43 +0530

    Changed start-server.bash to use local mongo db instead of docker

--- start-server.bash
+++ start-server.bash
@@ -1,10 +1,12 @@
 #! /bin/bash
 
-docker run -d --name mongo mongo
-export MONGODB_URI="$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mongo):27017"
+#docker run -d --name mongo mongo
+#export MONGODB_URI="$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mongo):27017"
 docker run -d --name kafka -p 9092:9092 apache/kafka:3.8.0
 docker run -d --name front -p 3000:3000 image-db-frontend
 bash ./gradlew bootRun --stacktrace
-docker stop mongo kafka front
-docker rm mongo kafka front
+#docker stop mongo kafka front
+#docker rm mongo kafka frontnt
+docker stop kafka front
+docker rm kafka front
 yes | docker system prune
