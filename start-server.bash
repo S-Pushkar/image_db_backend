3caabf092b23ab7ab39092b8e35944aed2cf8dfc
Pushkar S <spushkar0071234@gmail.com>
2024-10-06 05:48:31 +0530

    Used the frontend docker image and exported the mongo uri for portability

--- start-server.bash
+++ start-server.bash
@@ -1,8 +1,10 @@
 #! /bin/bash
 
 docker run -d --name mongo mongo
+export MONGODB_URI="$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mongo):27017"
 docker run -d --name kafka -p 9092:9092 apache/kafka:3.8.0
+docker run -d --name front -p 3000:3000 image-db-frontend
 bash ./gradlew bootRun --stacktrace
-docker stop mongo kafka
-docker rm mongo kafka
+docker stop mongo kafka front
+docker rm mongo kafka front
 yes | docker system prune
